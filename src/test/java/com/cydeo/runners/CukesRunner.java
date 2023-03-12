f689fd455b0d59bc63f24eaaa5cf42ab44a22948
KFrankPA <114306682+KFrankPA@users.noreply.github.com>
2023-03-12 15:55:28 -0400

    3/12/2023

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import java.util.List;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "",
         publish = true
 
 )
